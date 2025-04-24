9e2b145b4b44fbf6f0febf6973f6c0a2a3af6b99
Khant Thu Aung <wannathu9@gmail.com>
2025-04-24 23:13:03 +0800

    changes

--- obj/Debug/net8.0/MyGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930ff721829597d90a19d52a5dc9ead5e44cb146")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
